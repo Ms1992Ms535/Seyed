04fd1e93c15f4c822ed399f10e0be3c827481b16
Ms1992Ms535 <ms1992ms535@gmail.com>
2025-05-07 14:39:43 +0330

    20250507

--- Seyed/obj/Debug/net8.0/Seyed.AssemblyInfo.cs
+++ Seyed/obj/Debug/net8.0/Seyed.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seyed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4efe13e4739aad63353544d3b24a5f79919881b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seyed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seyed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
